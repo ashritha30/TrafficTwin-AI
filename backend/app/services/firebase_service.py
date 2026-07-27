49ad004cad378c36de6284f07bf113e7d6c15482
ashritha30 <polamadaashritha@gmail.com>
2026-07-27 19:55:05 +0530

    Support Firebase credentials on Render

--- backend/app/services/firebase_service.py
+++ backend/app/services/firebase_service.py
@@ -1,9 +1,19 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase-key.json")
-
 if not firebase_admin._apps:
+    firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+
+    if firebase_credentials:
+        # Running on Render
+        cred_dict = json.loads(firebase_credentials)
+        cred = credentials.Certificate(cred_dict)
+    else:
+        # Running locally
+        cred = credentials.Certificate("firebase-key.json")
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
